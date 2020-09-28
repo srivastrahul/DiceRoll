1ccf3b1b1207e322120ab5435f712d17dceceab0
Rahul Srivastava <rahul.srivastava@sanganan.in>
2020-09-28 22:09:47 +0530

    UI upgrade.

--- app/src/main/java/in/rahul/diceroll/MainActivity.kt
+++ app/src/main/java/in/rahul/diceroll/MainActivity.kt
@@ -3,7 +3,11 @@ package `in`.rahul.diceroll
 import `in`.rahul.diceroll.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +20,33 @@ class MainActivity : AppCompatActivity() {
         activityViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
         binding.activityViewModel = activityViewModel
         binding.lifecycleOwner = this
+
+    }
+
+    override fun onPostResume() {
+        super.onPostResume()
+
+        activityViewModel.isRolling.observe(this, Observer { isDiceRolling ->
+            if (isDiceRolling) {
+                val animationListener = object : Animation.AnimationListener {
+
+
+                    override fun onAnimationRepeat(animation: Animation?) {
+
+                    }
+
+                    override fun onAnimationEnd(animation: Animation?) {
+                        binding.diceIV.setImageResource(activityViewModel.diceNumberMLD.value!!)
+                    }
+
+                    override fun onAnimationStart(animation: Animation?) {
+
+                    }
+                }
+                val diceRoll: Animation = AnimationUtils.loadAnimation(this, R.anim.shake)
+                diceRoll.setAnimationListener(animationListener)
+                binding.diceIV.startAnimation(diceRoll)
+            }
+        })
     }
 }
--- app/src/main/java/in/rahul/diceroll/MainActivityViewModel.kt
+++ app/src/main/java/in/rahul/diceroll/MainActivityViewModel.kt
@@ -1,24 +1,26 @@
 package `in`.rahul.diceroll
 
+import android.widget.ImageView
 import androidx.databinding.Bindable
+import androidx.databinding.BindingAdapter
 import androidx.databinding.Observable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.squareup.picasso.Picasso
 import java.util.*
 
 class MainActivityViewModel : ViewModel(), Observable {
 
+    val diceNumberMLD: MutableLiveData<Int> = MutableLiveData()
+    val isRolling: MutableLiveData<Boolean> = MutableLiveData()
 
-    @Bindable
-    val diceNumberMLD = MutableLiveData<String>()
-
-    private var lastClickTime : Long = System.nanoTime()
-    private  var currentClickTime: Long = 0L
-
+    private var lastClickTime: Long = System.nanoTime()
+    private var currentClickTime: Long = 0L
 
     init {
-        diceNumberMLD.value = "1"
+        diceNumberMLD.value = R.drawable.six
+        isRolling.value = false
     }
 
     override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
@@ -29,7 +31,11 @@ class MainActivityViewModel : ViewModel(), Observable {
 
     }
 
+
+
+
     fun setCurrentClickTime() {
+        isRolling.value = true
         if (currentClickTime > 0L)
             lastClickTime = currentClickTime
         currentClickTime = System.nanoTime()
@@ -40,8 +46,17 @@ class MainActivityViewModel : ViewModel(), Observable {
         val n: Int = ((currentClickTime - lastClickTime) % 7L).toInt()
         if (n == 0)
             setCurrentClickTime()
-        else
-            diceNumberMLD.value = n.toString()
+        else {
+            isRolling.value = false
+            when (n) {
+                1 -> diceNumberMLD.value = R.drawable.one
+                2 -> diceNumberMLD.value = R.drawable.two
+                3 -> diceNumberMLD.value = R.drawable.three
+                4 -> diceNumberMLD.value = R.drawable.four
+                5 -> diceNumberMLD.value = R.drawable.five
+                6 -> diceNumberMLD.value = R.drawable.six
+            }
+        }
     }
 
 }
